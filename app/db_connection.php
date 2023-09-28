58e11f8c27b9f09061033f9afa1dbc8e0330bc4c
Adrimr7 <adrianmenaruiz@gmail.com>
2023-09-28 20:08:18 +0200

    Cambios PHP

--- app/db_connection.php
+++ app/db_connection.php
@@ -1,16 +1,25 @@
 <?php
 function OpenCon()
 {
+    $hostname = "db";
+    $username = "admin";
+    $password = "test";
+    $db = "database";
 
-$hostname = "db";
-$username = "admin";
-$password = "test";
-$db = "database";
-$conn = new mysqli($hostname, $username, $password, $database) or die("Connect failed: %s\n". $conn -> error);
-return $conn;
+    // Intenta establecer la conexión
+    $conn = new mysqli($hostname, $username, $password, $db);
+
+    // Verifica si se produjo un error en la conexión
+    if ($conn->connect_error) {
+        die("Database connection failed: " . $conn->connect_error);
+    }
+
+    return $conn;
 }
+
 function CloseCon($conn)
 {
-$conn -> close();
+    $conn->close();
 }
 ?>
+
