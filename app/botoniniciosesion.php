b71796eece22e05caf6931c2da9da22cd2fed6ac
Ander <santurmatica@gmail.com>
2023-10-09 20:11:42 +0200

    Arreglado el inicio de sesion

--- app/botoniniciosesion.php
+++ app/botoniniciosesion.php
@@ -6,12 +6,12 @@ $email = $_POST["email"];
 $contrasena = $_POST["contrasena"];
 
 // SQL para buscar el usuario por email y contraseña
-$sql = "SELECT COUNT(nombre) FROM usuarios WHERE email='$email' AND contraseña='$contrasena'";
-
+//$sql = "SELECT COUNT(nombre) FROM usuarios WHERE email='$email' AND contraseña='$contrasena'";
+$sql = "SELECT * FROM usuarios WHERE email='$email' AND contraseña='$contrasena'";
 $resultado = $conn->query($sql);
+$num_resultados = $resultado->num_rows; //Se hace una consulta y luego se cuentan las filas, con COUNT no deja
 
-if ($resultado > 0) {
-    
+if ($num_resultados > 0) {
     // Si hay mas de 0 lineas quiere decir que ha habido una busqueda exitosa por lo que deberia de ser un inicio correcto por correo y contraseña
     session_start();
     $_SESSION['autenticado'] = true;
@@ -21,7 +21,8 @@ if ($resultado > 0) {
 } 
 else {
     
-    echo "Inicio de sesión fallido";    
+    echo "<h1>Inicio de sesión fallido</h1>";   
+    include 'iniciosesion.php';
     exit;
 }
 $conn->close(); 
