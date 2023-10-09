2cb57fae81a8e69450e217aa829154750b7e400e
Etxaniz100 <eenneekkooeemm@gmail.com>
2023-10-09 19:18:57 +0200

    Comentarios varios

--- app/connection.php
+++ app/connection.php
@@ -1,13 +1,14 @@
 <?php
+//Preparamos los datos para la conexion con mysqli (una interfaz de MySQL para PHP)
 $hostname = "db";
 $username = "admin";
 $password = "test";
 $db = "database";
 
-
+//Abre la conexion al servidor de MySQL
 $conn = new mysqli($hostname, $username, $password, $db);
 
-
+//Si la concexion falla cerramos la concexion
 if ($conn->connect_error) {
   die("La conexión ha fallado: " . $conn->connect_error);
 }
--- app/index.php
+++ app/index.php
@@ -1,7 +1,11 @@
 <?php
+//Iniciamos la sesion de PHP 
 session_start();
-include 'connection.php';
+//Ejecuta el archivo connection.php
+include 'connection.php';      
+//Y lo mismo para la pagina HOME
 include 'home.html';
+//Cerramos la conexion con la base de datos
 $conn->close();
 
 
