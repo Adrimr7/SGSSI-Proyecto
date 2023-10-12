f6d33bc3517042ba5abacf84daaacfca49ad10d2
Jonmaa <jonmarcos1@gmail.com>
2023-10-12 18:39:14 +0200

    Lineas copiadas

--- app/vuelos.php
+++ app/vuelos.php
@@ -31,18 +31,33 @@ while ($row = $result->fetch_assoc())
             <div class='div-imagen'>
                 
                 <!-- Para cada elemento del vuelo, lo obtenemos de row y lo ponemos en esta especie de tabla-->
+                
+                <form id= 'origen$i' method='POST' action = 'modificarvuelo.php'>
+                <input type='hidden' name='origen' value='{$row['ciudad_salida']}'>
                     <b>Origen:</b> {$row['ciudad_salida']}<br>
+                </form>
+                
+                <form id= 'destino$i' method='POST' action = 'modificarvuelo.php'>
+                <input type='hidden' name='destino' value='{$row['ciudad_llegada']}'>
                     <b>Destino:</b> {$row['ciudad_llegada']}<br>
+                </form>
 
                     <!-- Como no sabia como pasar el callsign a php, he hecho que sea un form de un unico elemento y luego se hace submit del form-->
                     <!-- permitiendo tener el valor en el php, pero me da que no cuela-->
+                    
                     <form id= 'callsign$i' method='POST' action = 'eliminarvuelo.php'> 
                     	<input type='hidden' name='numcallsign' value='{$row['callsign']}'>
                         <b>CallSign:</b> <i id='callsign'>{$row['callsign']}</i><br>
                     </form>
-
+		
+	       <form id= 'pasajeros$i' method='POST' action = 'modificarvuelo.php'>
+	       <input type='hidden' name='numero_pasajeros' value='{$row['numero_pasajeros']}'>
                     <b>Nº Pasajeros:</b> {$row['numero_pasajeros']}<br>
+               </form>
+               <form id= 'fecha$i' method='POST' action = 'modificarvuelo.php'>
+               <input type='hidden' name='fecha' value='{$row['fecha']}'>
                     <b>Fecha:</b> {$row['fecha']}<br>
+	       </form>
 
                     <!-- Botones de editar y de eliminar-->
                     <button class='input' type='button' id='botModificar$i'>Modificar</button>
@@ -54,9 +69,17 @@ while ($row = $result->fetch_assoc())
                     <script>
                     document.getElementById('botModificar$i').addEventListener('click', function() 
                     {
-                    	document.getElementById('Formulario').submit();
-                        alert('Modificar');
-                        console.log('click');
+                    	var form = document.getElementById('origen$i');
+                    	var form1 = document.getElementById('destino$i');
+                    	var form2 = document.getElementById('callsign$i');
+                    	var form3 = document.getElementById('pasajeros$i');
+                    	var form4 = document.getElementById('fecha$i');
+        		form.submit();
+        		form1.submit();
+        		form2.submit();
+        		form3.submit();
+        		form4.submit();
+                    	        	
                     });
                     </script>
 
