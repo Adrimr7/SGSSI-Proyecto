2222c2161bf18c5b78d3aa2a36c581acb9562b5d
Jonmaa <jonmarcos1@gmail.com>
2023-10-12 10:12:46 +0200

    Intento de arreglar el eliminar pero no consigo pillar la variable callsign

--- app/eliminarvuelo.php
+++ app/eliminarvuelo.php
@@ -5,28 +5,15 @@ include 'connection.php';
 //Obtenemos los datos del submit usando los id de cada apartado
 $callSign = $_POST["callsign"];
 
+$sql = "DELETE FROM vuelo WHERE callsign = $callSign";
 
-//Este es un intento algo (bastante) mal planteado
-
-
-/*
-
-
-//Preparamos la instruccion SQL con los datos del formulario, en este caso una insercion de nuevo usuario
-$sql = "INSERT INTO usuarios (dni, nombre, apellidos, telefono, email, contraseña, nacimiento) VALUES ('$dni','$nombre','$apellidos','$telef','$email','$contraseña','$fnacimiento')";
-
-//A traves del objeto de conexion $conn ejecutamos query() para enviar la instruccion de SQL
 if ($conn->query($sql) === TRUE){
-		//Teniendo el usuario registrado, cerramos la conexion a la base de datos
-		$conn->close();
-		//Y pasamos a la pagina vuelos.php
-		include 'vuelos.php';
-
-		
-		exit;
-} else {
-	echo "Error: " . $conn->error;
-		$conn->close(); 
+	include 'vuelos.php';
+	exit;
 }
-*/
+else{
+	echo "Error al eliminar el vuelo";
+}
+
+$conn->close();
 ?>
