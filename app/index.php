7f53fa68df77c950037d4194f1edbb5b873eb7e1
Adrimr7 <adrianmenaruiz@gmail.com>
2023-10-16 19:04:03 +0200

    Sesion

--- app/index.php
+++ app/index.php
@@ -1,8 +1,6 @@
 <?php
-//Iniciamos la sesion de PHP 
-session_start();
 //Ejecuta el archivo connection.php
-include 'connection.php';      
+include 'connection.php';     
 //Y lo mismo para la pagina HOME
 include 'home.html';
 //Cerramos la conexion con la base de datos
