435b7a24bff2ac8978d7389134318776a525875b
Jonmaa <jonmarcos1@gmail.com>
2023-11-05 13:22:13 +0100

    ANTI-CSRF EN INICIO SESION

--- app/botoniniciosesion.php
+++ app/botoniniciosesion.php
@@ -7,48 +7,53 @@ $contraseña = $_POST["contrasena"];
 
 // Preparar la consulta para recuperar el hash y la salt
 $sql = "SELECT contraseña, salt FROM usuarios WHERE dni = ?";
+if (!empty($_POST['csrf_token']) && hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
+	if ($consulta = $conn->prepare($sql))
+	{
+	    //Asignamos el valor del dni al ? en la consulta
+	    $consulta->bind_param("s", $dni);
+	    $consulta->execute();
+	    $resultado = $consulta->get_result();
 
-if ($consulta = $conn->prepare($sql))
-{
-    //Asignamos el valor del dni al ? en la consulta
-    $consulta->bind_param("s", $dni);
-    $consulta->execute();
-    $resultado = $consulta->get_result();
+	    //Si obtenemos algun resultado
+	    if ($resultado->num_rows > 0) {
+		$fila = $resultado->fetch_assoc();
+		$hashAlmacenado = $fila['contraseña'];
+		$saltAlmacenado = $fila['salt'];
 
-    //Si obtenemos algun resultado
-    if ($resultado->num_rows > 0) {
-        $fila = $resultado->fetch_assoc();
-        $hashAlmacenado = $fila['contraseña'];
-        $saltAlmacenado = $fila['salt'];
+		// Concatenar la contraseña proporcionada con la salt almacenada
+		$contraseñaConSalt = $contraseña . $saltAlmacenado;
 
-        // Concatenar la contraseña proporcionada con la salt almacenada
-        $contraseñaConSalt = $contraseña . $saltAlmacenado;
-
-        if (password_verify($contraseñaConSalt, $hashAlmacenado)) {
-            // Contraseña verificada con éxito
-            // Inicia la sesión y realiza las acciones necesarias
-            session_start();
-            $_SESSION['autenticado'] = true;
-            $_SESSION['dni'] = $dni;
-            include 'vuelos.php';
-            exit;
-        } else {
-            echo '<script> alert("Contraseña incorrecta.");</script>';
-            include 'iniciosesion.php';
-            exit;
-        }
-    } else {
-        echo '<script> alert("Usuario no registrado.");</script>';
-        include 'iniciosesion.php';
-        exit;
-    }
-} 
-else
-{
-    // Gestion de errores de la consulta
-    echo '<script> alert("Error en la consulta");</script>';
-    include 'iniciosesion.php';
-    exit;
+		if (password_verify($contraseñaConSalt, $hashAlmacenado)) {
+		    // Contraseña verificada con éxito
+		    // Inicia la sesión y realiza las acciones necesarias
+		    session_start();
+		    $_SESSION['autenticado'] = true;
+		    $_SESSION['dni'] = $dni;
+		    include 'vuelos.php';
+		    exit;
+		} else {
+		    echo '<script> alert("Contraseña incorrecta.");</script>';
+		    include 'iniciosesion.php';
+		    exit;
+		}
+	    } else {
+		echo '<script> alert("Usuario no registrado.");</script>';
+		include 'iniciosesion.php';
+		exit;
+	    }
+	} 
+	else
+	{
+	    // Gestion de errores de la consulta
+	    echo '<script> alert("Error en la consulta");</script>';
+	    include 'iniciosesion.php';
+	    exit;
+	}
+}else{
+	echo '<script> alert("Error en el token CSRF");</script>';
+	include 'iniciosesion.php';
+	exit;
 }
 function generateHash($password) {
     return password_hash($password, PASSWORD_DEFAULT);
