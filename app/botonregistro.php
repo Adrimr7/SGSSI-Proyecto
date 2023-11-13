a1114832c6b1669f86d664aa7548be4fc1d9f6e5
Jonmaa <jonmarcos1@gmail.com>
2023-11-13 20:24:31 +0100

    reCAPTCHA hecho->Sol. diseño inseguro

--- app/botonregistro.php
+++ app/botonregistro.php
@@ -14,35 +14,47 @@ $contraseña = $_POST["contraseña"];
 $salt = generarSalt();
 $contraseñaConSalt = $contraseña . $salt;
 $hash = generarHash($contraseñaConSalt);
-if (!empty($_POST['csrf_token']) && hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
 
-	// Preparamos la instrucción SQL con los datos del formulario
-	$sql = "INSERT INTO usuarios (dni, nombre, apellidos, telefono, email, contraseña, salt, nacimiento) VALUES (?,?,?,?,?,?,?,?)";
+// Verifica reCAPTCHA
+$recaptchaSecretKey = "6Le-Iw4pAAAAAKy2E0RvBBi4jpMBXjmkE6MxoPgY";
+$response = $_POST['g-recaptcha-response'];
+$verify = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$recaptchaSecretKey}&response={$response}");
+$data = json_decode($verify);
 
-	if ($consulta = $conn->prepare($sql))
-	{
-	    //Asignamos el valor del dni al ? en la consulta
-	    $consulta->bind_param("sssissss", $dni, $nombre, $apellidos, $telef, $email, $hash, $salt, $fnacimiento);
-	    $resultado = $consulta->execute();
-	    
-	    if($resultado) {
-	    	echo '<script> alert("Cuenta creada con exito.");</script>';
-	    	include 'iniciosesion.php';
-		exit;
-	    } else {
-	    	echo '<script> alert("Se ha producido un error al crear la cuenta.");</script>';
-	    } 
-	}else
-	{
-	    // Gestion de errores de la consulta
-	    echo '<script> alert("Error en la consulta");</script>';
-	    include 'registro.php';
-	    $consulta->close();
-	    $conn->close();
-	    exit;
+
+if ($data->success) {
+	if (!empty($_POST['csrf_token']) && hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
+
+		// Preparamos la instrucción SQL con los datos del formulario
+		$sql = "INSERT INTO usuarios (dni, nombre, apellidos, telefono, email, contraseña, salt, nacimiento) VALUES (?,?,?,?,?,?,?,?)";
+
+		if ($consulta = $conn->prepare($sql))
+		{
+		    //Asignamos el valor del dni al ? en la consulta
+		    $consulta->bind_param("sssissss", $dni, $nombre, $apellidos, $telef, $email, $hash, $salt, $fnacimiento);
+		    $resultado = $consulta->execute();
+		    
+		    if($resultado) {
+		    	echo '<script> alert("Cuenta creada con exito.");</script>';
+		    	include 'iniciosesion.php';
+			exit;
+		    } else {
+		    	echo '<script> alert("Se ha producido un error al crear la cuenta.");</script>';
+		    } 
+		}else
+		{
+		    // Gestion de errores de la consulta
+		    echo '<script> alert("Error en la consulta");</script>';
+		    include 'registro.php';
+		    $consulta->close();
+		    $conn->close();
+		    exit;
+		}
+	}else{
+		echo "Error token CSRF";	
 	}
 }else{
-	echo "Error token CSRF";	
+	echo "Error con el reCAPTCHA. Reintentalo";
 }
 function generarHash($password) {
     return password_hash($password, PASSWORD_DEFAULT);
